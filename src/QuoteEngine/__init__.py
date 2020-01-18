c55483ec6b60f7f1eecc9e937876ac9780d1567a
Carlos Loureda <balanzeneto@gmail.com>
2020-01-18 14:40:34 +0100

    [app.py] Made app.py serve '/' and '/create' routes
    On path '/' user will be able to see random generated memes
    On path '/create' user will be able to create a custom meme

--- src/QuoteEngine/__init__.py
+++ src/QuoteEngine/__init__.py
@@ -1,1 +1,2 @@
 from .QuoteModel import QuoteModel
+from .Ingestor import Ingestor
--- src/app.py
+++ src/app.py
@@ -1,32 +1,35 @@
+"""Flask server file. We will be serving a '/'  and a '/create'  endpoints so \
+the user can see random created memes or create a custom one."""
+
 import random
 import os
 import requests
-from flask import Flask, render_template, abort, request
+from flask import Flask, render_template, abort, request, jsonify
 
-from typing import List
+from typing import List, Tuple
 from PIL import Image
 
 from QuoteEngine import Ingestor
-# @TODO Import your MemeEngine classes
+from MemeGenerator import MemeEngine
 
 
 app = Flask(__name__)
 
-# meme = MemeEngine('./static')
+meme = MemeEngine('./static')
 
 
-# TODO: Add this to utils or to a class ? Use the pythons standard library os class to find all
+# @TODO: Add this to utils or to a class ? Use the pythons standard library os class to find all
 # images within the images images_path directory
 
 
-def get_images(path: str) -> List[Image]:
-    """Get all the images in path
+def get_images(path: str) -> List[str]:
+    """Get all the images in path.
 
     Arguments:
         path {str} -- The path where to look for images
 
     Return:
-        List[Image] -- A List of iamges on Pillow format
+        List[str] -- A List of images paths
     """
     imgs = []
     valid_images = [".jpg", ".gif", ".png", ".tga"]
@@ -34,14 +37,17 @@ def get_images(path: str) -> List[Image]:
         ext = os.path.splitext(f)[1]
         if ext.lower() not in valid_images:
             continue
-        imgs.append(Image.open(os.path.join(path, f)))
-
-    print(imgs)
+        imgs.append(os.path.join(path, f))
     return imgs
 
 
-def setup():
-    """Load all resources."""
+def setup() -> Tuple[List[str], List[str]]:
+    """Load all resources.
+
+    Returns:
+        (quotes, imgs)  { Tuple[List[str], List[str]] } -- Get all the quotes
+                                                        and Images to work with
+    """
     quote_files = ['./_data/DogQuotes/DogQuotesTXT.txt',
                    './_data/DogQuotes/DogQuotesDOCX.docx',
                    './_data/DogQuotes/DogQuotesPDF.pdf',
@@ -60,41 +66,69 @@ def setup():
 
 
 quotes, imgs = setup()
-# print(quotes, imgs)
 
 
 @app.route('/')
 def meme_rand():
-    """Generate a random meme"""
-    # @TODO:
-    # Use the random python standard library class to:
-    # 1. select a random image from imgs array
-    # 2. select a random quote from the quotes array
+    """Generate a random meme.
 
-    img = None
-    quote = None
+    Returns:
+        The result of rendering the `meme.html` template
+    """
+    img = random.choice(imgs)
+    quote = random.choice(quotes)
     path = meme.make_meme(img, quote.body, quote.author)
     return render_template('meme.html', path=path)
 
 
 @app.route('/create', methods=['GET'])
 def meme_form():
-    """ User input for meme information """
+    """User input for meme information.
+
+    Returns:
+        The result of rendering the `meme_form.html` template
+    """
     return render_template('meme_form.html')
+
+# https://i.redd.it/me6ijotn0kq31.jpg
+# I love you 3000
+# Tony Stark
+
+    # image_url: https://i.redd.it/me6ijotn0kq31.jpg
 
 
 @app.route('/create', methods=['POST'])
 def meme_post():
-    """ Create a user defined meme """
+    """Create a user defined meme.
 
-    # @TODO:
+    Returns:
+        The result of rendering the `meme.html` template
+    """
+    if not request or not request.form:
+        raise Exception("Not proper request")
+
+        # @TODO:
     # 1. Use requests to save the image from the image_url
     #    form param to a temp local file.
+    data = request.form.to_dict(flat=True)
+    if data["image_url"] == "" or data["body"] == "":
+        # @TODO: Add or send some errors here. Also make the previous form
+        #  check on the clinet
+        print("Error on required images")
+        raise Exception(
+            "Sorry human! I need an image and a quote for builing a meme for you :)")
+
+    image_url = data["image_url"]
+    image_tmp_path = f'./tmp/{image_url[image_url.rfind("/") +1:]}'
+    r = requests.get(image_url, allow_redirects=False)
+
+    open(image_tmp_path, 'wb').write(r.content)
+
     # 2. Use the meme object to generate a meme using this temp
     #    file and the body and author form paramaters.
+    path = meme.make_meme(image_tmp_path, data["body"], data["author"])
     # 3. Remove the temporary saved image.
-
-    path = None
+    os.remove(image_tmp_path)
 
     return render_template('meme.html', path=path)
 
